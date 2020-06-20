f1fc434d388f5a1f76d4e9310e96422d933d9d6d
Frank D. Macias-Escriva <huranku@gmail.com>
2020-06-19 20:42:01 -0700

    Divided subapertures
    Assigned subapertures to matrix

--- Process_data_jt1.m
+++ Process_data_jt1.m
@@ -16,3 +16,21 @@ plot(detrend(squeeze(proc_data.mag(1,1,:))-2^15));
 %%   
 RenderOrthoSlice(proc_data,'Jet','log');
 %SetImageThreshold(proc_data.mag);
+
+z_slice = proc_data.mag(:,:,1024); 
+%%
+figure(11)
+imagesc(log10(z_slice));
+%%
+k_slice = fft2(z_slice-mean(z_slice,'all'));
+%subtract mean to remove DC
+%%
+figure(12)
+imagesc(fftshift(abs(k_slice)));
+%%
+whole_aperture = zeros(1500,1500);
+whole_aperture(376:1125, 376:1125) = k_slice;
+
+figure(13)
+imagesc(fftshift(abs(whole_aperture)));
+%%
