30a1892e7dc90a51c652e471bf3a06a0b576844e
jackcwtang <jackcwtang@gmail.com>
2020-07-07 13:42:37 -0700

    Create Get_filenames.m
    This records all filenames in a directory. Used to create the key for blinded filenames in Ido's dataset.

---
+++ Get_filenames.m
@@ -0,0 +1,7 @@
+%% Get data file names
+% Jack Tang
+% 2020 07 04 happy birthday America
+
+allFiles = dir('F:\Shared drives\JSO Lab Team Drive\Projects\Synapse Analysis\Confocal Image Stacks\Jack\*\');
+allNames = {allFiles.name};
+
