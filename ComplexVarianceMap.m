7f2b1b60e1e7481b7aa41b468dab6cccef6f9358
jackcwtang <jackcwtang@gmail.com>
2020-07-18 14:24:38 -0700

    Added dummy DVV file

--- ComplexVarianceMap.m
+++ ComplexVarianceMap.m
@@ -11,5 +11,29 @@
 %   Last modified by: Jack C. Tang
 %   Last modified on: 2020-07-14
 
+<<<<<<< HEAD
+%% Create dummy DVV file for Santec systems.
+SantecDVV(1280)
+dvv=readNPY('DVV.npy')
+
+%% Input the raw OCT data
+option.command = '';
+OCT_data = ProcessSpectralInterferogramSantec(4096, option);
+
+%% Combine magnitude and phase to get the complex signal
+complex_data = OCT_data.mag.*exp(1i.*OCT_data.phase);
+
+%% Denoise and create a mask for the image
+noise_roi.x_start = 1;
+noise_roi.x_end = 10;
+noise_roi.y_start = 2;
+noise_roi.y_end = 99;
+[denoised, masks] = TM_segmentation(noise_roi, 'results/');
+
+%% Compute and display the variance map
+V = squeeze(var(complex_data,0,2));
+image(transpose(log(V)));
+=======
 option.command = '';
 ProcessSpectralInterferogram(4096, option);
+>>>>>>> 8de53b929ddb7acb2a7126a5a3039154b4edfa73
